02a917a8c9e526153956ad0889e24a486ceb3a90
tugalsan karabacak <tugalsan@gmail.com>
2023-04-15 08:18:23 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,11 @@
 module com.tugalsan.tst.serialcom {
     requires gwt.user;
     requires com.tugalsan.api.log;
-    requires com.tugalsan.api.console;
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.random;
     requires com.tugalsan.api.time;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.cast;
-    requires com.tugalsan.api.file;
     requires com.tugalsan.api.serialcom;
     requires com.fazecast.jSerialComm;
 }
