c7bdad921b223c4eba5d33544f6af79f39dbdb77
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-08 10:39:49 +0300

    .

--- src/main/java/com/tugalsan/tst/serialcom/Main.java
+++ src/main/java/com/tugalsan/tst/serialcom/Main.java
@@ -4,7 +4,7 @@ import com.tugalsan.api.serialcom.kincony.server.KC868_A32_R1_2.*;
 
 //WHEN RUNNING IN NETBEANS, ALL DEPENDENCIES SHOULD HAVE TARGET FOLDER!
 //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.serialcom
-//java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.tst.serialcom-1.0-SNAPSHOT-jar-with-dependencies.jar    
+//java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.serialcom-1.0-SNAPSHOT-jar-with-dependencies.jar    
 public class Main {
 
     public static void main(String... s) {
